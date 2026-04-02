376df87aac268a4a7750b15bc548e1770b67e6bc
Punoi Vilaivanh <pvilaivanh@gmail.com>
2026-04-02 18:33:11 -0400

    final challenge

--- setup.sh
+++ setup.sh
@@ -5,11 +5,17 @@ docker exec -i mydb psql -U postgres <<EOF
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   name TEXT,
-  email TEXT
+  email TEXT,
+  age INT
 );
 
-INSERT INTO users (name, email)
-VALUES ('Charlie', 'charlie@test.com')
+ALTER TABLE users ADD COLUMN IF NOT EXISTS age INT;
+
+INSERT INTO users (name, email, age)
+VALUES 
+('David', 'david@test.com', 25),
+('Emma', 'emma@test.com', 30),
+('Frank', 'frank@test.com', 28)
 ON CONFLICT DO NOTHING;
 
 EOF
